ea9ed7823abffe2c59e52e72416e955b051a3d37
Mistralys <s.mordziol@gmail.com>
2023-06-30 18:22:48 +0200

    Added including of files.

--- src/DocParser.php
+++ src/DocParser.php
@@ -10,9 +10,12 @@ declare(strict_types=1);
 
 namespace Mistralys\MarkdownViewer;
 
+use AppUtils\ConvertHelper;
 use AppUtils\FileHelper;
+use AppUtils\FileHelper\FileInfo;
 use GeSHi;
 use ParsedownExtra;
+use testsuites\FileHelperTests\PathInfoTest;
 
 /**
  * Markdown document parser. Uses "ParseDown extra" to parse the
@@ -23,9 +26,13 @@ use ParsedownExtra;
  */
 class DocParser
 {
+    public const VALIDATION_INCLUDE_FILE_NOT_FOUND = 140801;
+    public const VALIDATION_INCLUDE_IS_NOT_FOLDER = 140802;
+    public const VALIDATION_INCLUDE_FILE_TOO_BIG = 140803;
+
     public const OVERALL_CLASS = 'geshifilter';
 
-    private string $html;
+    private ?string $html = null;
     private DocFile $file;
 
     /**
@@ -38,20 +45,44 @@ class DocParser
      *
      * @var array<string,string>
      */
-    private $langAliases = array(
+    private array $langAliases = array(
         'js' => 'javascript',
         'html' => 'html5',
         'json' => 'javascript'
     );
 
+    /**
+     * @var string[]
+     */
+    private array $includeExtensions = array(
+        'md',
+        'txt'
+    );
+
+    /**
+     * @var array<int,array<int,string>>
+     */
+    private array $includes = array();
+    private int $maxIncludeSize = 6000;
+
     public function __construct(DocFile $file)
     {
+        $this->file = $file;
+    }
+
+    private function parse() : void
+    {
+        if(isset($this->html)) {
+            return;
+        }
+
         $parse = new ParsedownExtra();
 
-        $text = FileHelper::readContents($file->getPath());
-        $text = $this->parseListStyles($text);
+        $text = FileHelper::readContents($this->file->getPath());
+        $text = $this->preParseListStyles($text);
+        $text = $this->preParseDetectIncludeFiles($text);
+        $text = $this->preParseInjectIncludes($text);
 
-        $this->file = $file;
         $this->html = (string)$parse->text($text);
 
         $this->parseHeaders();
@@ -63,6 +94,8 @@ class DocParser
      */
     public function getHeaders() : array
     {
+        $this->parse();
+
         return $this->headers;
     }
 
@@ -73,7 +106,7 @@ class DocParser
      * @param string $text
      * @return string
      */
-    private function parseListStyles(string $text) : string
+    private function preParseListStyles(string $text) : string
     {
         $lines = explode("\n", $text);
 
@@ -102,6 +135,134 @@ class DocParser
         }
 
         return $line;
+    }
+
+    private function preParseDetectIncludeFiles(string $text) : string
+    {
+        if(stripos($text, '{include-file:') === false) {
+            return $text;
+        }
+
+        preg_match_all('/{include-file:([^{}]+)}/iU', $text, $matches);
+
+        if(isset($matches[0][0]))
+        {
+            foreach($matches[0] as $idx => $matchedText)
+            {
+                $file = trim($matches[1][$idx]);
+                if(!isset($this->includes[$file])) {
+                    $this->includes[$file] = array();
+                }
+
+                if(!in_array($matchedText, $this->includes[$file], true)) {
+                    $this->includes[$file][] = $matchedText;
+                }
+            }
+        }
+
+        return $text;
+    }
+
+    private function preParseInjectIncludes(string $text) : string
+    {
+        $basePath = dirname($this->file->getPath());
+        $replaces = array();
+
+        $text = str_replace(
+            array(
+                '\{',
+                '\}'
+            ),
+            array(
+                '§__BRACKET_OPEN__§',
+                '§__BRACKET_CLOSE__§'
+            ),
+            $text
+        );
+
+        foreach($this->includes as $relativePath => $textMatches)
+        {
+            $content = $this->loadIncludeContent($basePath.'/'.$relativePath);
+
+            foreach($textMatches as $matchedText)
+            {
+                $replaces[$matchedText] = $content;
+            }
+        }
+
+        $text = str_replace(
+            array_keys($replaces),
+            array_values($replaces),
+            $text
+        );
+
+        // Restore escaped brackets
+        return str_replace(
+            array(
+                '§__BRACKET_OPEN__§',
+                '§__BRACKET_CLOSE__§'
+            ),
+            array(
+                '{',
+                '}'
+            ),
+            $text
+        );
+    }
+
+    private function loadIncludeContent(string $path) : string
+    {
+        if(!file_exists($path)) {
+            return $this->renderErrorMessage(
+                sprintf(
+                    'Include file `%s` not found.',
+                    basename($path)
+                ),
+                self::VALIDATION_INCLUDE_FILE_NOT_FOUND
+            );
+        }
+
+        if(is_dir($path)) {
+            return $this->renderErrorMessage(
+                sprintf(
+                    'Include path `%s` is not a file.',
+                    basename($path)
+                ),
+                self::VALIDATION_INCLUDE_IS_NOT_FOLDER
+            );
+        }
+
+        if(filesize($path) > $this->maxIncludeSize) {
+            return $this->renderErrorMessage(
+                sprintf(
+                    'Include file `%s` is too big. Max file size is %s.',
+                    basename($path),
+                    ConvertHelper::bytes2readable($this->maxIncludeSize)
+                ),
+                self::VALIDATION_INCLUDE_FILE_TOO_BIG
+            );
+        }
+
+        return FileInfo::factory($path)->getContents();
+    }
+
+    private function renderErrorMessage(string $message, int $code) : string
+    {
+        return sprintf('**Error #%s:** %s', $code, $message).PHP_EOL;
+    }
+
+    /**
+     * Returns all detected include files. The keys are the
+     * relative paths to the target file, the values are indexed
+     * array with the matched <code>{include}</code> commands.
+     *
+     * @return array<string,array<int,string>>
+     */
+    public function getIncludes() : array
+    {
+        $this->parse();
+
+        return $this->includes;
     }
 
     private function parseHeaders() : void
@@ -179,6 +340,60 @@ class DocParser
 
     public function render() : string
     {
+        $this->parse();
+
         return $this->html;
     }
+
+    // region: Options
+
+    public function setMaxIncludeSize(int $bytes) : self
+    {
+        $this->maxIncludeSize = $bytes;
+        return $this;
+    }
+
+    public function getMaxIncludeSize() : int
+    {
+        return $this->maxIncludeSize;
+    }
+
+    /**
+     * Adds a file extension to allow for include files.
+     *
+     * @param string $extension
+     * @return $this
+     */
+    public function addIncludeExtension(string $extension) : self
+    {
+        $extension = strtolower(ltrim($extension, '.'));
+
+        if(!in_array($extension, $this->includeExtensions, true)) {
+            $this->includeExtensions[] = $extension;
+        }
+
+        return $this;
+    }
+
+    public function addIncludeExtensions(...$extensions) : self
+    {
+        foreach($extensions as $extension)
+        {
+            if(is_array($extension)) {
+                $this->addIncludeExtensions(...$extension);
+                continue;
+            }
+
+            $this->addIncludeExtension($extension);
+        }
+
+        return $this;
+    }
+
+    public function getIncludeExtensions() : array
+    {
+        return $this->includeExtensions;
+    }
+
+    // endregion
 }
