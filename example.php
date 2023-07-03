c17ec6bd8e3aec8ba8489f2689f7003768d563c7
Mistralys <s.mordziol@gmail.com>
2023-07-03 12:04:31 +0200

    Added configuration.

--- example.php
+++ example.php
@@ -1,21 +1,25 @@
 <?php
 
-    declare(strict_types=1);
+declare(strict_types=1);
 
-    use Mistralys\MarkdownViewer\DocsManager;
-    use Mistralys\MarkdownViewer\DocsViewer;
+use Mistralys\MarkdownViewer\DocsManager;
+use Mistralys\MarkdownViewer\DocsViewer;
+use Mistralys\MarkdownViewer\DocsConfig;
 
-    if(!file_exists('vendor/autoload.php')) {
-        die('Please run <code>composer install</code> first.');
-    }
+if(!file_exists(__DIR__.'/vendor/autoload.php')) {
+    die('Please run <code>composer install</code> first.');
+}
 
-    require_once 'vendor/autoload.php';
+require_once __DIR__.'/vendor/autoload.php';
 
-    $manager = (new DocsManager())
-        ->addFile('Package readme', 'README.md');
+$config = (new DocsConfig())
+    ->addIncludePath(__DIR__.'/tests/files/includes')
+    ->addIncludeExtension('php');
 
-    (new DocsViewer($manager, 'vendor'))
-        ->setTitle('Markdown viewer')
-        ->setPackageURL('./')
-        ->display();
+$manager = (new DocsManager($config))
+    ->addFile('Package readme', 'README.md');
 
+(new DocsViewer($manager, 'vendor'))
+    ->setTitle('Markdown viewer')
+    ->setPackageURL('./')
+    ->display();
